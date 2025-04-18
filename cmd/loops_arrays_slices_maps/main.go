b2501c53cf023c0a3b57513631ad79c4c543c80c
Pew-X <piyushwaghmare69420@gmail.com>
2025-04-18 16:37:33 +0530

    Slices & some pointer tomfoolery

--- cmd/loops_arrays_slices_maps/main.go
+++ cmd/loops_arrays_slices_maps/main.go
@@ -11,5 +11,23 @@ func main() {
 	arr[0] = seed
 	fmt.Println(arr[0])
 	fmt.Println(seed)
+	fmt.Println(&arr[0])
+
+	addr := &arr[0]
+
+	//Slices
+
+	slice, seed := loops_arrays_slices_maps.Slice(7)
+	slice[0] = seed
+	slice[1] = *addr
+
+	fmt.Println(slice[0])
+	fmt.Println(seed)
+	fmt.Println(&slice[0])
+
+	// second element of size will be the the elemet that is at addr of arr[0]
+	fmt.Println(slice[1])
+	fmt.Println(addr)
+	fmt.Println(&slice[1])
 
 }
--- loops_arrays_slices_maps/loops_arrays_slices_maps.go
+++ loops_arrays_slices_maps/loops_arrays_slices_maps.go
@@ -2,8 +2,13 @@ package loops_arrays_slices_maps
 
 // Arrays - Fixed size , same type , indexable and contiguous memory
 
-func Arrays(seed int) ([5]int, int) {
-	var arr [5]int
+func Arrays(seed int32) ([5]int32, int32) {
+	var arr [5]int32
 	return arr, seed
 
+}
+
+func Slice(seed int32) ([]int32, int32) {
+	arr := make([]int32, seed)
+	return arr, seed
 }
